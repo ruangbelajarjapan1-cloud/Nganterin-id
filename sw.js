d038d6c485b03f991d515efe014610042591e929
ruangbelajarjapan1-cloud <ruangbelajarjapan1@gmail.com>
2026-06-23 09:34:16 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = "nganterin-v1";
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/food.html",
-  "/ride.html",
-  "/send.html",
-  "/mart.html",
-  "/driver.html",
+  "./",
+  "./index.html",
+  "./food.html",
+  "./ride.html",
+  "./send.html",
+  "./mart.html",
+  "./driver.html",
   "https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap",
   "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
 ];
